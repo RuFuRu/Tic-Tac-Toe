289d90ce4fd2fd7d335eaf5819bb7c276b9a6d87
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-06-07 20:18:55 +0200

    renamed certain arrays

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ console.log(gameboardDivs);
 const gameboard = (() => {
     //"p" in gameboardArr stands for "placeholder"
     const gameboardArr = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'];
-    const gameboardFirstCol = ['p', 'p', 'p'];
-    const gameboardSecondCol = ['p', 'p', 'p'];
-    const gameboardThirdCol = ['p', 'p', 'p'];
+    const gameboardArrFirstCol = ['p', 'p', 'p'];
+    const gameboardArrSecondCol = ['p', 'p', 'p'];
+    const gameboardArrThirdCol = ['p', 'p', 'p'];
     let counter = 0;
     for (let i = 0; i < gameboardDivs.length; i++) {
         gameboardDivs[i].addEventListener('click', () => {
@@ -25,21 +25,21 @@ const gameboard = (() => {
                 counter++;
                 console.log(counter);
             }
-            gameboardFirstCol[0] = gameboardArr[0];
-            gameboardFirstCol[1] = gameboardArr[3];
-            gameboardFirstCol[2] = gameboardArr[6];
-            gameboardSecondCol[0] = gameboardArr[1];
-            gameboardSecondCol[1] = gameboardArr[4];
-            gameboardSecondCol[2] = gameboardArr[7];
-            gameboardThirdCol[0] = gameboardArr[2];
-            gameboardThirdCol[1] = gameboardArr[5];
-            gameboardThirdCol[2] = gameboardArr[8];
+            gameboardArrFirstCol[0] = gameboardArr[0];
+            gameboardArrFirstCol[1] = gameboardArr[3];
+            gameboardArrFirstCol[2] = gameboardArr[6];
+            gameboardArrSecondCol[0] = gameboardArr[1];
+            gameboardArrSecondCol[1] = gameboardArr[4];
+            gameboardArrSecondCol[2] = gameboardArr[7];
+            gameboardArrThirdCol[0] = gameboardArr[2];
+            gameboardArrThirdCol[1] = gameboardArr[5];
+            gameboardArrThirdCol[2] = gameboardArr[8];
             if (getOccurance(gameboardArr.slice(0, 3), 'X') === 3 ||
                 getOccurance(gameboardArr.slice(3, 6), 'X') === 3 ||
                 getOccurance(gameboardArr.slice(6), 'X') === 3 ||
-                getOccurance(gameboardFirstCol, 'X') === 3 ||
-                getOccurance(gameboardSecondCol, 'X') === 3 ||
-                getOccurance(gameboardThirdCol, 'X') === 3) {
+                getOccurance(gameboardArrFirstCol, 'X') === 3 ||
+                getOccurance(gameboardArrSecondCol, 'X') === 3 ||
+                getOccurance(gameboardArrThirdCol, 'X') === 3) {
                 console.log("'X' wins!");
                 gameboardDivs.forEach(gameboardDiv => {
                     gameboardDiv.setAttribute('style', 'pointer-events: none');
@@ -48,9 +48,9 @@ const gameboard = (() => {
             else if (getOccurance(gameboardArr.slice(0, 3), 'O') === 3 ||
                 getOccurance(gameboardArr.slice(3, 6), 'O') === 3 ||
                 getOccurance(gameboardArr.slice(6), 'O') === 3 ||
-                getOccurance(gameboardFirstCol, 'O') === 3 ||
-                getOccurance(gameboardSecondCol, 'O') === 3 ||
-                getOccurance(gameboardThirdCol, 'O') === 3) {
+                getOccurance(gameboardArrFirstCol, 'O') === 3 ||
+                getOccurance(gameboardArrSecondCol, 'O') === 3 ||
+                getOccurance(gameboardArrThirdCol, 'O') === 3) {
                 console.log("'O' wins!");
                 gameboardDivs.forEach(gameboardDiv => {
                     gameboardDiv.setAttribute('style', 'pointer-events: none');
