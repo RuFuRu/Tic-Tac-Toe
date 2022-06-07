b53d0509050c788b364e1bd05ba54beef416f182
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-06-07 18:40:40 +0200

    added basic tic-tac-toe functionallity

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 "use strict";
-const gameBoard = () => {
-    const gameBoardArr = ['x', 'O', 'x'];
-    return {};
-};
+const gameboardElement = document.querySelector('.gameboard');
+const gameboardDivs = document.querySelectorAll('.gameboard > div');
+console.log(gameboardDivs);
+const gameboard = (() => {
+    const gameboardArr = ['x', 'O', 'x'];
+    let counter = 1;
+    return { counter };
+})();
+for (let i = 0; i < gameboardDivs.length; i++) {
+    gameboardDivs[i].addEventListener('click', () => {
+        if (gameboard.counter % 2 === 0) {
+            const circle = document.createElement('p');
+            circle.textContent = "O";
+            gameboardDivs[i].appendChild(circle);
+            gameboardDivs[i].setAttribute('style', 'pointer-events: none');
+            gameboard.counter++;
+        }
+        else if (gameboard.counter % 2 !== 0) {
+            const x = document.createElement('p');
+            x.textContent = "X";
+            gameboardDivs[i].appendChild(x);
+            gameboardDivs[i].setAttribute('style', 'pointer-events: none');
+            gameboard.counter++;
+        }
+    });
+}
--- script.ts
+++ script.ts
@@ -1,4 +1,28 @@
-const gameBoard = () => {
-    const gameBoardArr: string[] = ['x','O','x'];
-    return {};
+const gameboardElement = document.querySelector('.gameboard') as HTMLDivElement;
+const gameboardDivs = document.querySelectorAll('.gameboard > div');
+console.log(gameboardDivs);
+
+const gameboard = (() => {
+    const gameboardArr: string[] = ['x','O','x'];
+    let counter = 1;
+    return {counter};
+})();
+
+for(let i = 0; i < gameboardDivs.length; i++) {
+    gameboardDivs[i].addEventListener('click', () => {
+        if(gameboard.counter % 2 === 0) {
+            const circle = document.createElement('p');
+            circle.textContent = "O";
+            gameboardDivs[i].appendChild(circle);
+            gameboardDivs[i].setAttribute('style', 'pointer-events: none');
+            gameboard.counter++;
+        }
+        else if(gameboard.counter % 2 !== 0) {
+            const x = document.createElement('p');
+            x.textContent = "X";
+            gameboardDivs[i].appendChild(x);
+            gameboardDivs[i].setAttribute('style', 'pointer-events: none');
+            gameboard.counter++;
+        }
+    })
 }
